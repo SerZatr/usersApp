66087514d9028b99b716a036d5758d2a3724ca27
SerZatr <90468588+SerZatr@users.noreply.github.com>
2025-04-14 04:12:55 +0300

    Update store.ts

--- src/shared/store/store.ts
+++ src/shared/store/store.ts
@@ -29,25 +29,20 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const setupStore = () => {
-  const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      }).concat(usersApi.middleware),
-  });
-  const persistor = persistStore(store);
-  return { store, persistor };
-};
-
-export const store = setupStore();
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(usersApi.middleware),
+});
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof rootReducer>;
-export type TAppStore = ReturnType<typeof setupStore>;
-export type TDispatch = TAppStore["dispatch"];
+export type TAppStore = typeof store;
+export type TDispatch = typeof store.dispatch;
 
 export const useAppDispatch = () => useDispatch<TDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
