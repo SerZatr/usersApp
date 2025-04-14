13ceceefd1b44fba53bbb9690ece66ecef4b5ee7
SerZatr <90468588+SerZatr@users.noreply.github.com>
2025-04-14 04:28:47 +0300

    Update index.tsx

--- src/pages/main/userCards/index.tsx
+++ src/pages/main/userCards/index.tsx
@@ -4,25 +4,32 @@ import { CardsList } from "../../../shared/ui/cardsList";
 import style from "./userCards.module.scss";
 import { NoData } from "../../../shared/ui/noData";
 import { UseGetFilteredUsers } from "./UseGetFilteredUsers";
+import { useAppSelector } from "../../../shared/store/store";
+import { Loader } from "../../../shared/ui/loader";
 
 export const UserCards = () => {
   const filteredUsers = UseGetFilteredUsers();
+  const areUsersLoaded = useAppSelector((store) => store.users.areUsersLoaded);
+
+  if (!areUsersLoaded) {
+    return <Loader />;
+  }
+
+  if (!Object.keys(filteredUsers ?? {}).length) {
+    return <NoData />;
+  }
 
   return (
     <section className={style.userCards}>
-      {Object.keys(filteredUsers).length ? (
-        <CardsList>
-          {filteredUsers.map((user) => (
-            <NavLink to={`user/?id=${user.id}`} key={String(user.id)}>
-              <Card>
-                <span>{user.name}</span>
-              </Card>
-            </NavLink>
-          ))}
-        </CardsList>
-      ) : (
-        <NoData />
-      )}
+      <CardsList>
+        {filteredUsers.map((user) => (
+          <NavLink to={`user/?id=${user.id}`} key={String(user.id)}>
+            <Card>
+              <span>{user.name}</span>
+            </Card>
+          </NavLink>
+        ))}
+      </CardsList>
     </section>
   );
 };
